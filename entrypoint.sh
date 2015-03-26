f6f7a37d3ab3d666911d8ba2af35500bf3662fce
tanaka0323 <tanaka@infocorpus.com>
2015-03-26 17:39:22 +0900

    Change to edit config file when first run only.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,15 @@ if [ "DB_REPLICA_NAME" = "" ]; then
     DB_PORT = false
 fi
 
-# Configure RockMongo
-sed -i "s|DB_HOST|$DB_HOST|
-        s/DB_PORT/$DB_PORT/
-        s/DB_USER/$DB_USER/
-        s/DB_PASSWORD/$DB_PASSWORD/
-        s/DB_AUTH/$DB_AUTH/
-        s/DB_REPLICA_NAME/$DB_REPLICA_NAME/g" /var/www/html/config.php
+RM_FIRST=/firstrun
+
+if [ ! -f $RM_FIRST ]; then
+    # Configure RockMongo
+    sed -i "s|DB_HOST|$DB_HOST|
+            s/DB_PORT/$DB_PORT/
+            s/DB_USER/$DB_USER/
+            s/DB_PASSWORD/$DB_PASSWORD/
+            s/DB_AUTH/$DB_AUTH/
+            s/DB_REPLICA_NAME/$DB_REPLICA_NAME/g" /var/www/html/config.php
+    touch $RM_FIRST
+fi
